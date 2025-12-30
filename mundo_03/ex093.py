f21158e13819dad07bb8abb6b556c04671605404
Eduardo Zafra <duduzafra@gmail.com>
2025-12-30 21:40:42 +0100

    finished ex093 and ex094

---
+++ mundo_03/ex093.py
@@ -0,0 +1,19 @@
+# Crie um programa que gerencie o aproveitamento de um jogador de futebol.
+# O programa vai ler o nome do jogador e quantas partidas ele jogou.
+# Depois vai ler a quantidade de gols feitos em cada partida.
+# No final, tudo isso será guardado em um dicionário, incluindo o total de gols feitos durante o campeonato.
+
+jogador = {'nome': str(input('Nome do Jogador: ')).strip().capitalize()}
+partidas_jogadas =  int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
+jogador['gols'] = [int(input(f'Quantos gols na partida {i}? ')) for i in range(partidas_jogadas)]
+jogador['total'] = sum(jogador['gols'])
+print('-=' * 20)
+print(jogador)
+print('-=' * 20)
+for k, v in jogador.items():
+    print(f'O campo {k} tem o valor {v}')
+print('-=' * 20)
+print(f'O jogador {jogador["nome"]} jogou {partidas_jogadas} partidas.')
+for i, gols in enumerate(jogador['gols']):
+    print(f'\t=> Na partida {i}, fez {gols} gols.')
+print(f'Foi um total de {jogador["total"]} gols.')
---
+++ mundo_03/ex094.py
@@ -0,0 +1,49 @@
+# Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um dicionário e
+# todos os dicionários em uma lista. No final, mostre:
+# A) Quantas pessoas foram cadastradas
+# B) A média de idade
+# C) Uma lista com as mulheres
+# D) Uma lista de pessoas com idade acima da média
+
+from statistics import mean
+
+pessoas = []
+while True:
+    pessoa = dict()
+    pessoa['nome'] = str(input('Nome: ')).strip().capitalize()
+    pessoa['sexo'] = str(input('Sexo: [M/F] ').strip().upper())
+    while len(pessoa['sexo']) != 1 or pessoa['sexo'] not in 'MF':
+        print('\033[31merror:\033[m Por favor, digite apenas M ou F.')
+        pessoa['sexo'] = str(input('Sexo: [M/F] ').strip().upper())
+    pessoa['idade'] = str(input('Idade: ')).strip()
+    while not pessoa['idade'].isdigit():
+        print('\033[31merror:\033[m Por favor, digite um valor numérico.')
+        pessoa['idade'] = str(input('Idade: ')).strip()
+    pessoa['idade'] = int(pessoa['idade'])
+    pessoas.append(pessoa)
+
+    validation = str(input('Quer continuar? [S/N] ')).strip().upper()
+    while len(validation) != 1 or validation not in 'SN':
+        print('\033[merror:\033[m Responda apenas S ou N.')
+        validation = str(input('Quer continuar? [S/N] ')).strip().upper()
+    if validation == 'N':
+        break
+print('-=' * 20)
+print(f'A) Ao todo temos {len(pessoas)} cadastradas.')
+print(f'B) A média de idade é de {mean(list(pessoa['idade'] for pessoa in pessoas)):.1f} anos.')
+mulheres_cadastradas = [pessoa['nome'] for pessoa in pessoas if pessoa['sexo'] == 'F']
+if mulheres_cadastradas:
+    print('C) As mulheres cadastradas foram', end=' ')
+    for nome in mulheres_cadastradas:
+        print(nome, end=' ')
+    print('.\n')
+else:
+    print('C) Nenhuma mulher foi cadastrada.')
+print('D) Lista de pessoas com idade acima da média:')
+media_idade = mean(list(pessoa['idade'] for pessoa in pessoas))
+for pessoa in pessoas:
+    if pessoa['idade'] > media_idade:
+        for k, v in pessoa.items():
+            print(f'\t{k} = {v};', end=' ')
+        print()
+print('<< ENCERRADO >>')
